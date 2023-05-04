96b33c481ebd4c4b8e221c51901eba9c73a55e3a
mai4ok <gusakowvlad@gmail.com>
2023-05-04 18:38:14 +0300

    groups

--- OOPLessonExampleProject/main.cpp
+++ OOPLessonExampleProject/main.cpp
@@ -1,12 +1,30 @@
-﻿#include "students.h"
-
-int sum(int, int);
+﻿#include"manager.h"
 
 int main() {
-	Student st("Artyoma", "XXX");
 
+	Student s1("Ivan", "Ivanov", 14, 7.6);
+	Student s2("Alex", "Invisible", 15, 10);
+	Student s3("Peter", "Pen", 12, 4);
+	Student s4("Harry", "Potter", 14, 9.6);
+	Student s5("Alice", "Inder", 13, 8);
 
+	Group group;
+	group.add(s1);
+	group.add(s2);
+	group.add(s3);
+	group.add(s4);
+	group.add(s5);
 
+	Manager manager;
+
+	cout << " Group average age  " << manager.calcStudentsAvgAge(group) << endl;
+	cout << " Group average mark  " << manager.calcStudentsAvgMark(group) << endl;
+
+	Student s = manager.findBestStudents(group);
+	cout << "Best group student " << s.convert() << endl;
+
+	s = manager.findWorstStudents(group);
+	cout << "Worst group student " << s.convert() << endl;
 
 	return 0;
 }
--- OOPLessonExampleProject/manager.h
+++ OOPLessonExampleProject/manager.h
@@ -1,1 +1,7 @@
 #pragma once
+#include "students.h"
+
+class Manager {
+
+
+};
